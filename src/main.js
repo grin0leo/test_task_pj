d3bc402a3b66c8693ee28eb9fd0d896152e12e31
grin_leo <gridchin.leo@gmail.com>
2025-04-02 15:16:56 +0300

    scrollToTop

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import './style.css'
 import { setupFormHandler } from "./js/formHandler.js";
 import { modalLogick } from './js/modalChangeLocation.js';
+import { scrollToTop } from './js/backToTop.js';
 
 
-
+// прокрутка к началу страницы
+const backToTopButton = document.querySelector('.back-to-top');
+backToTopButton.addEventListener('click', scrollToTop);
 
 // Валидация формы отправки номера + маска номера + succes/error message
-
-
 document.addEventListener("DOMContentLoaded", function () {
   setupFormHandler();
   modalLogick()
